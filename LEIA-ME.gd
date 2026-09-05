b2671bb5f954ddc1f06d2d9412db88743ac26482
P4 <p4bl0eduardo2011@gmail.com>
2026-09-05 16:18:10 -0300

    versão estavel-1 dos primeiros testes

--- LEIA-ME.gd
+++
@@ -1,6 +0,0 @@
-Preciso definir sobre o que vai ser o jogo estou mais inclinado a escolher plataforma mas
-variar no estilo de jogo como um *visto de cima*, sem ser copia de mario ta bom.
-
-1. não ser tão simples
-2. ter pelo menos fases ou um bom numero de telas/ mapa grandinho
-3.
--- Player/player.gd
+++ Player/player.gd
@@ -1,30 +1,32 @@
 extends CharacterBody2D
 
-const SPEED = 900.0
-const JUMP_FORCE = -400.0
+const JUMP_FORCE = -600.0
+
+@onready var hud: Control = $"../CanvasLayer/HUD"
 
 var contador: int = 0
 
-@onready var hud = $"../CanvasLayer/HUD"
-
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity += get_gravity() * delta * 1.5
 
 	if Input.is_action_just_pressed("ui_up") and is_on_floor():
 		velocity.y = JUMP_FORCE
 
 	var direction := Input.get_axis("ui_left", "ui_right")
+	var running = Input.is_action_pressed("ui_shift")
+	var BASE_SPEED = 950.0 if running else 680.0
+	
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * BASE_SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, BASE_SPEED)
 
 	move_and_slide()
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body == self:
-		var limite = 2
+		var limite: int = 2
 		if contador > limite-1:
 			if contador == limite:
 				global_scale = 1.5 * global_scale
