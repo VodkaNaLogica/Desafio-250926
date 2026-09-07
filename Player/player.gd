f735c329a839b46fd02f98ba5d594804c4ff727e
P4 <p4bl0eduardo2011@gmail.com>
2026-09-07 11:59:25 -0300

    tentando adicionar uma nova movimentação e interação com a hud

--- Player/player.gd
+++ Player/player.gd
@@ -1,26 +1,41 @@
 extends CharacterBody2D
 
 const JUMP_FORCE = -600.0
-
-@onready var hud: Control = $"../CanvasLayer/HUD"
+var global_direction
 
 var contador: int = 0
+var idModo: int = 0
 
 func _physics_process(delta: float) -> void:
-	if not is_on_floor():
-		velocity += get_gravity() * delta * 1.5
-
-	if Input.is_action_just_pressed("ui_up") and is_on_floor():
-		velocity.y = JUMP_FORCE
-
-	var direction := Input.get_axis("ui_left", "ui_right")
 	var running = Input.is_action_pressed("ui_shift")
 	var BASE_SPEED = 950.0 if running else 680.0
+	var BASE_ROTATION = 15
 	
-	if direction:
-		velocity.x = direction * BASE_SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, BASE_SPEED)
+	if idModo == 0:
+		if not is_on_floor():
+			velocity += get_gravity() * delta * 1.5
+		if Input.is_action_just_pressed("ui_up") and is_on_floor():
+			velocity.y = JUMP_FORCE
+
+		var direction := Input.get_axis("ui_left", "ui_right")
+			
+		if direction:
+			velocity.x = direction * BASE_SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, BASE_SPEED)
+
+	if idModo == 1:
+		var directionX := Input.get_axis("ui_left", "ui_right")
+		var directionY := Input.get_axis("ui_down", "ui_up")
+	
+	
+		if directionX:
+			rotation += directionX / BASE_ROTATION
+		else:
+			rotation += directionX / BASE_ROTATION
+
+		if directionY:
+			position += Vector2.UP.rotated(rotation) * BASE_SPEED * delta
 
 	move_and_slide()
 
@@ -33,4 +48,9 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 				contador+=1
 		else:
 			contador += 1
-			hud.atualizarLabel(str(contador) + "/" + str(limite))
+			Hud.atualizarLabel(str(contador) + "/" + str(limite), 1)
+
+func mudarDeModo():
+	if idModo > 1:
+		idModo = 0
+	idModo += 1
--- hud.gd
+++ hud.gd
@@ -1,6 +1,15 @@
 extends Control
 
-@onready var panelLabel = $Panel/Label
+@onready var panelLabel1 = $"Panel/HBoxContainer/Label"
+@onready var panelLabel2 = $"Panel/HBoxContainer/Label2"
 
-func atualizarLabel(text: String):
-	panelLabel.text = text
+func atualizarLabel(text: String, id: int):
+	var strId = str(id)
+	match  strId:
+		"1":
+			panelLabel1.text = text
+		"2":
+			panelLabel2.text = text
+
+func _on_button_pressed() -> void:
+	Player.mudarDeModo()
