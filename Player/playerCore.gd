669f3ca49631d2265ebfadeb1d803742ed3ec26c
P4 <p4bl0eduardo2011@gmail.com>
2026-09-12 01:58:15 -0300

    Organização, tamanho e posições de salas e corredores consertadas e ideias de hud desenvolvidas e novos problemas criados e mentalmente resolvidos

--- Player/playerCore.gd
+++ Player/playerCore.gd
@@ -1,10 +1,12 @@
 extends CharacterBody2D
 
+var estamina
+
 func _physics_process(_delta: float) -> void:
 	var running = Input.is_action_pressed("ui_shift")
-	var baseSpeed = 110 if running else 73
+	var baseSpeed = 140 if running else 100
 	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-
+	
 	velocity = direction * baseSpeed
 
 	move_and_slide()
---
+++ progress_bar.gd
@@ -0,0 +1,4 @@
+extends ProgressBar
+
+func _ready() -> void:
+	value = 100
